5b681fbed004ad1a7b103b37adb018f7db152821
Hasnain Syed <hasnainalsyed@gmail.com>
2023-08-15 20:29:24 +0500

    Our Services Section Done

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 document.documentElement.style.setProperty('--header-height', document.getElementById('site-header').offsetHeight + 'px');
+
+let swiper = new Swiper("[our-services-swiper]", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
